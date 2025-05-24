54bd5dd89fefa6692ab5c900407bb9919968ca0a
WIN-ST5EU1T38E7\Administrator <2649340476@qq.com>
2025-05-24 18:20:25 +0800

    feat: 编写着色器为三角形上色(https://www.yuque.com/u34333378/oevi5k/nqnk851da6aeqh32)

--- OpenGLDemo/src/Application.cpp
+++ OpenGLDemo/src/Application.cpp
@@ -3,6 +3,54 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#include <string>
+#include <vector>
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    // TODO:: Error Handling
+    int res;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &res); // 获取着色器得编译结果
+    if (res == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length); // 获取着色器日志信息得长度
+        // char* errorLog = (char*)alloca(sizeof(char) * length); // 分配栈内存
+        std::vector<char> errorLog(length);
+        glGetShaderInfoLog(id, length, &length, &errorLog[0]); // 获取着色器日志信息
+        std::cout << "Failed to compile "
+            << (type == GL_VERTEX_SHADER ? "vertex" : "fragment")
+            << " shader: "
+            << errorLog.data()
+            << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& VertexShader, const std::string& FragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, VertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, FragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program); // 验证有效性
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
 
 int main(void)
 {
@@ -41,10 +89,33 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), Pos, GL_STATIC_DRAW);
 
-    // 启用索引为0得顶点属性
+    // 启用索引为0得顶点属性（绑定顶点位置信息）
     glEnableVertexAttribArray(0);
     // 设置索引为0得顶点属性数据，这将决定着色器如何在一组顶点缓存数据中读取索引为0得数据（数据绑定）
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    unsigned int shaderProgram = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shaderProgram);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -60,6 +131,9 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteBuffers(1, &buffer); // 清理缓存
+    glDeleteProgram(shaderProgram); // 清理着色器程序
 
     glfwTerminate();
     return 0;
